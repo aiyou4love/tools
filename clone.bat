996d3262839242976cf58f0396cb8033476f5c49
aiyou4love <aiyou4love@outlook.com>
2016-05-26 18:51:54 +0800

    u

--- clone.bat
+++ clone.bat
@@ -1,8 +1,33 @@
 @echo off
-curl -o LuaJIT-2.1.0-beta2.zip http://luajit.org/download/LuaJIT-2.1.0-beta2.zip
-7z.exe x LuaJIT-2.1.0-beta2.zip
-cd LuaJIT-2.1.0-beta2
+tools/curl.exe -o LuaJIT-2.1.0-beta2.zip http://luajit.org/download/LuaJIT-2.1.0-beta2.zip
+tools/7z.exe x LuaJIT-2.1.0-beta2.zip
+cd LuaJIT-2.1.0-beta2/src
+msvcbuild.bat
+cd ../..
+del /q /s /f LuaJIT-2.1.0-beta2.zip
+mkdir include
+mkdir lib
+mkdir bin
+mkdir include/lua
+xcopy /r /y LuaJIT-2.1.0-beta2/src/luaconf.h include/lua/
+xcopy /r /y LuaJIT-2.1.0-beta2/src/lua.h include/lua/
+xcopy /r /y LuaJIT-2.1.0-beta2/src/lauxlib.h include/lua/
+xcopy /r /y LuaJIT-2.1.0-beta2/src/lualib.h include/lua/
+xcopy /r /y LuaJIT-2.1.0-beta2/src/luajit.h include/lua/
+xcopy /r /y LuaJIT-2.1.0-beta2/src/lua.hpp include/lua/
+xcopy /r /y LuaJIT-2.1.0-beta2/src/lua51.lib lib/
+xcopy /r /y LuaJIT-2.1.0-beta2/src/lua51.dll bin/
+rd /s /q LuaJIT-2.1.0-beta2
 git clone https://github.com/valnoel/rapidxml.git
-git clone git://code.csdn.net/Tencent/rapidjson.git
+mkdir include/rapidxml
+xcopy /r /y rapidxml/*.hpp include/rapidxml/
+rd /s /q rapidxml
+git clone https://code.csdn.net/Tencent/rapidjson.git
+xcopy /r /e /y rapidjson/include/rapidjson include/
+rd /s /q rapidjson
 git clone https://github.com/curl/curl.git
+cd curl\winbuild
+nmake /f Makefile.vc mode=static VC=12 ENABLE_WINSSL=yes MACHINE=x64
+xcopy /r /e /y curl\builds\libcurl-vc12-x64-release-static-ipv6-sspi-winssl/include/ include/
+xcopy /r /y curl\builds\libcurl-vc12-x64-release-static-ipv6-sspi-winssl/libcurl_a.lib lib/
 @echo on
