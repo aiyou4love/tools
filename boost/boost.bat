060746685bef4f5f931c2b7d2e6c769c7c35554f
aiyou4love <aiyou4love@outlook.com>
2016-05-30 10:11:43 +0800

    u

--- boost/boost.bat
+++ boost/boost.bat
@@ -4,11 +4,10 @@ tools\7z.exe x boost_1_61_0.7z
 del /q /s /f boost_1_61_0.7z
 cd boost_1_61_0
 call bootstrap.bat
-b2 toolset=msvc address-model=64 link=static runtime-link=static stage
+b2 toolset=msvc address-model=64 stage
 cd ..
 mkdir include\boost
 xcopy /r /y boost_1_61_0\stage\lib\*.lib lib\
 xcopy /r /e /y boost_1_61_0\boost include\boost\
-rd /s /q boost_1_61_0
 rd /s /q boost_1_61_0
 @echo on
--- curl/curl.bat
+++ curl/curl.bat
@@ -4,7 +4,7 @@ cd curl\winbuild
 nmake /f Makefile.vc mode=static VC=14 ENABLE_WINSSL=yes MACHINE=x64
 cd ..\..
 xcopy /r /e /y curl\builds\libcurl-vc14-x64-release-static-ipv6-sspi-winssl\include include\
-xcopy /r /y curl\builds\libcurl-vc14-x64-release-static-ipv6-sspi-winssl\lib\libcurl_a.lib lib\
+copy /y curl\builds\libcurl-vc14-x64-release-static-ipv6-sspi-winssl\lib\libcurl_a.lib lib\libcurl.lib
 rd /s /q curl
 
 @echo on
