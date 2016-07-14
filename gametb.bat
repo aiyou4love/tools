a85e017eff569c254b2e388eadeccf42c59fb967
zyouhua <zyouhua@gmail.com>
2016-07-14 08:03:00 +0800

    u

--- gametb.bat
+++ gametb.bat
@@ -2,13 +2,25 @@
 cd ../bin
 if not exist asset (
 	mkdir asset
+	cd asset
+	mkdir table
+	cd ..
+)
+if not exist log (
+	mkdir log
 )
 if not exist update (
 	mkdir update
 )
+if not exist plugin (
+	mkdir plugin
+)
 if not exist storage (
 	mkdir storage
 )
+if not exist download (
+	mkdir download
+)
 cd ../
-command\consoleui\bin\Release\consoleui.exe -t gameTb bin/asset
+command\consoleui\bin\Release\consoleui.exe -t gameTb bin/asset/table
 @echo on
